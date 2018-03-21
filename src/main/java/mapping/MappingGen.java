7f2e1d59dfb9464442bcb2f174e1cb486445c455
Retarded-Skid <waffleiron42@yahoo.com>
2018-03-21 07:02:32 -0400

    Update mapping-gen to support command-line argument usage

--- src/main/java/mapping/MappingGen.java
+++ src/main/java/mapping/MappingGen.java
@@ -10,12 +10,26 @@ public class MappingGen {
 
 	public static void main(String[] args) {
 		try {
+			// default version
 			String ver = "1.12.2";
-			//String s = new MCPOnlineJsonGen(ver, false, "snapshot").createJSON();
-			String s = new MCPJsonGen(ver, false).createJSON();
+			if (args.length > 0) {
+				ver = args[0];
+			}
+			// generator type
+			AbstractJsonGen gen = null;
+			if (args.length > 1) {
+				String type = args[1];
+				if (type.equals("mcp") || type.equals("online")) {
+					gen = new MCPOnlineJsonGen(ver, false, "snapshot");
+				} else {
+					gen = new MCPJsonGen(ver, false);
+				}
+			}
+			// print version mapping via generator
+			String s = gen.createJSON();
 			System.out.println(s);
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.err.println("Failure to generate mappings: " + e.getMessage());
 		}
 	}
 
